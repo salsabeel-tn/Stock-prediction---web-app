dd1917f76c00fe69c4b73ded115eb764e8211585
salsabeel_tnt <salsabeel@breeze-technologies.de>
2022-10-24 01:55:40 +0300

    finishg off

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import pandas as pd
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -23,7 +24,8 @@ def plot_raw_data():
     fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
-st.title("Stock Prediction WebApp")
+
+st.title("Stock Prediction Web App")
 
 stocks = ('AAPL',"GOOG","MSFT","GME")
 
@@ -37,11 +39,31 @@ data_load_state = st.text("Loading data....")
 data = load_data(selected_stock)
 data_load_state.text("Successfully loaded the data!")
 
-st.subheader('Raw data')
-st.write(data.tail())
+st.subheader('Summary Statistics on the stock values')
+st.write(data.describe())
 
 plot_raw_data()
 
-#todo: Forecasting
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train['Date'] = df_train['Date'].dt.tz_localize(None)
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"}) # renaming columns to meet Prophet's naming convention
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
 
 
